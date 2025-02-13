d09a4b3becf4d57b0170f5ffef274c5333458e3a
SerhiiDziuba1309 <doktorschuuu@gmail.com>
2025-02-13 20:22:17 +0100

    add email and reset password

--- src/constants/tokens.js
+++ src/constants/tokens.js
@@ -1,5 +1,6 @@
 
 import crypto from 'crypto';
+import path from 'node:path';
 
 export const generateToken = () => crypto.randomBytes(32).toString('hex');
 
@@ -12,4 +13,14 @@ export const SORT_ORDER = {
   export const FIFTEEN_MINUTES = 15*60*1000;
   export const ONE_MONTH = 30*24*60*60*1000;
   
-  
+
+
+export const SMTP = {
+  SMTP_HOST: 'SMTP_HOST',
+  SMTP_PORT: 'SMTP_PORT',
+  SMTP_USER: 'SMTP_USER',
+  SMTP_PASSWORD: 'SMTP_PASSWORD',
+  SMTP_FROM: 'SMTP_FROM',
+};
+
+export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,12 +1,15 @@
-import { validateBody } from "../middlewares/validateBody.js";
+import { validateBody, } from "../middlewares/validateBody.js";
+import {ctrlWrapper} from "../utils/ctrlWrapper.js"
 import { Router } from "express";
 import { 
     registerUserController,
-     loginUserController, 
-     refreshSessionController,
-     logoutUserController
-     } from "../controllers/authController.js";
-import {registerSchema, loginSchema} from "../schemas/authSchemas.js"
+    loginUserController, 
+    refreshSessionController,
+    logoutUserController,
+    requestResetEmailController,
+    resetPasswordController
+} from "../controllers/authController.js";
+import {registerSchema, loginSchema, requestResetEmailSchema, resetPasswordSchema} from "../schemas/authSchemas.js"
 
 const authRouter = Router();
 
@@ -14,5 +17,15 @@ authRouter.post('/register', validateBody(registerSchema), registerUserControlle
 authRouter.post('/login', validateBody(loginSchema), loginUserController);
 authRouter.post('/refresh', refreshSessionController);
 authRouter.post('/logout', logoutUserController);
+
+authRouter.post('/request-reset-email', 
+validateBody(requestResetEmailSchema),
+ctrlWrapper(requestResetEmailController));
+
+authRouter.post(
+    '/reset-password',
+    validateBody(resetPasswordSchema),
+    ctrlWrapper(resetPasswordController),
+)
 
 export default authRouter
